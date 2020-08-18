9b31103bd0f1cf58353b08fcf80e0b0c7bbc3a56
pms131 <sportssk@naver.com>
2020-08-18 17:41:43 +0900

    ch 02 p99

--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -10,8 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 class TacoCloudApplicationTests {
 	
 	@Autowired
@@ -25,5 +26,12 @@ class TacoCloudApplicationTests {
 			   .andExpect(content().string(containsString("Welcome to...")));
 		
 	}
+	
+	/*
+	 * 여기에 넣는 것도 가능 (@WebMvcConfigurer 추가시) 
+	 @Override 
+	 public void addViewControllers(ViewControllerRegistry registry) {
+	 registry.addViewController("/").setViewName("home"); }
+	 */
 
 }
